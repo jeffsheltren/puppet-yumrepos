1b6581106343a2859e036f199841ee3187ca5ac0
dwooldridge <david.wooldridge@intudigital.co.uk>
2015-04-23 14:39:49 +0100

    Added Varnish 4 repo

--- manifests/params.pp
+++ manifests/params.pp
@@ -19,7 +19,7 @@ class yumrepos::params {
   $elrepo_testing_enabled = '0'
   $elrepo_testing_gpgcheck = '1'
   $elrepo_testing_includepkgs = absent
-  $elrepo_testing_exclude = absent 
+  $elrepo_testing_exclude = absent
 
   $elrepo_kernel_url = "http://elrepo.org/linux/kernel/el${::operatingsystemmajrelease}/${::architecture}"
   $elrepo_kernel_enabled = '0'
@@ -96,6 +96,13 @@ class yumrepos::params {
   $varnish3_gpgcheck = '0'
   $varnish3_includepkgs = absent
   $varnish3_exclude = absent
+
+  # Varnish 4 Settings.
+  $varnish4_url = "http://repo.varnish-cache.org/redhat/varnish-4.0/el${::operatingsystemmajrelease}/${::architecture}"
+  $varnish4_enabled = '1'
+  $varnish4_gpgcheck = '0'
+  $varnish4_includepkgs = absent
+  $varnish4_exclude = absent
 
   # Repoforge Settings.
   $repoforge_url = "http://apt.sw.be/redhat/el${::operatingsystemmajrelease}/en/${::architecture}/rpmforge"
